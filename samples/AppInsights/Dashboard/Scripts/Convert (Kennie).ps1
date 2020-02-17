834a52344b4549c096858f28b1b1286920cd1be6
Freddy Kristiansen <freddyk@microsoft.com>
2020-02-17 23:35:04 +0100

    Kennies Dashboard

---
+++ samples/AppInsights/Dashboard/Scripts/Convert (Kennie).ps1
@@ -0,0 +1,9 @@
+﻿. (Join-Path $PSScriptRoot "ConvertExportedDashboardToDashboardTemplate.ps1") `
+    -ExportedDashboardFile "C:\Users\freddyk\Downloads\KennieTestWS.json" `
+    -TemplateFile "C:\Users\freddyk\Downloads\Kennie Test Dashboard.json" `
+    -myAppInsightsSubscription "0eac3279-98e8-4f24-bc7e-217785e4dfdd" `
+    -myAppInsightsResourceGroup "moagar-test-PartnerTelemetry" `
+    -myAppinsightsName "AppInsightsClient1" `
+    -myDashboardSubscription "" `
+    -myDashboardResourceGroup "" `
+    -myDashboardResourceId ""
