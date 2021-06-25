aa51fc8eefc974a71e1ed4dd78ffe3cf5ba1d649
Henrik Westergaard Hansen <henrikwh@microsoft.com>
2021-06-25 13:29:48 +0200

    Adding API.ReadWrite.All scenario

--- samples/PSOAuthBCAccess/AcquireToken-AsApplication.ps1
+++ samples/PSOAuthBCAccess/AcquireToken-AsApplication.ps1
@@ -9,8 +9,14 @@ Write-Host "Enter the secret for client $ClientId :"
 $ClientSecret = Read-Host -AsSecureString
 
 # acquire access token as application with client id and secret
-$AuthenticationResult = Get-MsalToken -ClientId $ClientId -ClientSecret $ClientSecret -TenantId $AadTenantId -Authority $AuthorityUri -Scopes $BcAutomationScopes
+$AuthenticationResult = Get-MsalToken -ClientId $ClientId -ClientSecret $ClientSecret -TenantId $AadTenantId -Authority $AuthorityUri -Scopes $BcApplicationScopes
 
-# use access token to call BC automation web service
+
+# use access token to call BC web service
+$BcResponse = Invoke-BCWebService -RequestUrl $SampleBCS2SUrl -AccessToken $AuthenticationResult.AccessToken
+Write-BCWebServiceResponse -Response $BcResponse
+
+
+# use access token to call BC *automation* web service
 $BcResponse = Invoke-BCWebService -RequestUrl $SampleBCAutomationUrl -AccessToken $AuthenticationResult.AccessToken
 Write-BCWebServiceResponse -Response $BcResponse
