a3fd404cbcbedec6118e376fefba960a0f62900e
chrishdmicrosoftcom <chrishdmicrosoftcom@users.noreply.github.com>
2021-07-01 14:11:53 +0000

    Switch to admin center sdk (#71)
    * Switched AdminCenter C# sample to use the SDK
    
    * Updated method names
    
    * newer nuget
    
    * Simpler authentication + renames

--- samples/AdminCenterApi/CSharp/Program.cs
+++ samples/AdminCenterApi/CSharp/Program.cs
@@ -6,20 +6,16 @@ namespace CSharp
 {
     class Program
     {
-        // Authentication parameters
-        const string aadAppId = "a19cb26a-2e4c-408b-82e1-6311742ecc50";         // partner's AAD app id
-        const string aadAppRedirectUri = "http://localhost";                    // partner's AAD app redirect URI
-        const string aadTenantId = "f5b6b245-5dd2-4bf5-94d4-35ef04d73c6d";      // customer's tenant id
-
         static void Main()
         {
             // Create a token credential, which enables us to authenticate to the Business Central Admin Center APIs.
-            // Note: this will open the AAD login page in a browser window.
+            //   Note 1: This will open the AAD login page in a browser window.
+              // Note 2: You can also skip passing in options altogether if you want to log into your own Business Central admin center, i.e., not a delegated admin scenario
             var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
             {
-                ClientId = aadAppId,
-                RedirectUri = new Uri(aadAppRedirectUri),
-                TenantId = aadTenantId,
+                ClientId = "a19cb26a-2e4c-408b-82e1-6311742ecc50",  // partner's AAD app id
+                RedirectUri = new Uri("http://localhost"),          // partner's AAD app redirect URI
+                TenantId = "f5b6b245-5dd2-4bf5-94d4-35ef04d73c6d",  // customer's tenant id
             };
             var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
 
